c03db628cb4e3c93d4dff539d3427fc7ccbf6cff
Abdullah <125198163+abdullahdilekkk@users.noreply.github.com>
2025-08-18 16:50:17 +0300

    navbar güncellendi

--- Demo/Demo/urls.py
+++ Demo/Demo/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('Turkey/', include("Map.urls")),
+    path('Map/', include("Map.urls")),
 ]
--- Demo/Map/urls.py
+++ Demo/Map/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.turkey_only, name="TurkeyPage"),
+    path('Turkey', views.turkey_only, name="TurkeyPage"),
 ]
