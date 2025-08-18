0667f35a769aeac2099c9d6b11bf60ddfbaf96cd
Abdullah <125198163+abdullahdilekkk@users.noreply.github.com>
2025-08-18 17:22:36 +0300

    sıkıntılar var

--- Demo/Map/urls.py
+++ Demo/Map/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('Turkey', views.turkey_only, name="TurkeyPage"),
+    path('Turkey/Istanbul', views.istanbul_only, name="IstanbulPage"),
 ]
