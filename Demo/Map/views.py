c608a141da3703b67457e03a97e7a3f0334f6c35
Abdullah <125198163+abdullahdilekkk@users.noreply.github.com>
2025-08-18 17:22:36 +0300

    sıkıntılar var

--- Demo/Map/views.py
+++ Demo/Map/views.py
@@ -5,6 +5,12 @@ import folium
 
 # Create your views here.
 def turkey_only(request):
-    m = folium.Map(location=[39.0, 35.0], zoom_start=6, width="50%", height="100px")
+    m = folium.Map(location=[39.0, 35.0], zoom_start=6, width="100%", height="600px")
     html = m.get_root().render()
     return render(request, "Map/Turkey.html", {"Turkey":html})
+
+def istanbul_only(request):
+    m = folium.Map(location=[41.0082, 28.9784], zoom_start=10, width="100%", height="600px")
+    html = m._repr_html_()
+    return render (request, "Map/istanbul.html", {"Istanbul":html})
+
