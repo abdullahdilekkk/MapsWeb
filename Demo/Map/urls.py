aef7035014e54cc659348154a7792ba1f82bd9e0
Abdullah <125198163+abdullahdilekkk@users.noreply.github.com>
2025-08-19 11:31:24 +0300

    büyükşehirlerin hepsi eklendi urls dinamikleştirildi

--- Demo/Map/urls.py
+++ Demo/Map/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('Turkey', views.turkey_only, name="TurkeyPage"),
-    path('Turkey/istanbul', views.istanbul_only, name="IstanbulPage"),
+    path('Turkey', views.turkey_only),
+    path('Turkey/<str:city>', views.cities_only),
 ]
