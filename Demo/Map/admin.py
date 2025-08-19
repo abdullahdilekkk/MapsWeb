0fc375190cb975a61ca6e1db4a1efe003e57d165
Abdullah <125198163+abdullahdilekkk@users.noreply.github.com>
2025-08-19 15:00:47 +0300

    81 il düzgünce geliyor

--- Demo/Map/admin.py
+++ Demo/Map/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
-
+from . import models
+import os, csv
+from django.conf import settings
 # Register your models here.
+
+@admin.register(models.City)
+class CitiesAdmin(admin.ModelAdmin):
+    list_display = ("name", "qx", "qy", "is_metropolitan", "country")
+    list_filter = ("name", "is_metropolitan", "country")
+    search_fields = ("name", "country")
+    actions = ["loadCSV"]
+
+    def loadCSV(self, request, queryset):   
+            path = os.path.join(settings.BASE_DIR, "turkey_cities.csv")
+            with open(path) as file:
+                for row in csv.DictReader(file):
+                    models.City.objects.get_or_create(
+                        name = row["name"],
+                        qx = float(row["qx"]),
+                        qy = float(row["qy"]),
+                        is_metropolitan = True if row["is_metropolitan"] == "True" else False ,
+                        country = row["country"]
+                    )
+    
+
+    
+    loadCSV.short_description = "Cities Load"
+
+
--- Demo/Map/models.py
+++ Demo/Map/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
 # Create your models here.
+
+class City(models.Model):
+    name = models.CharField(max_length=156)
+    qx = models.FloatField()
+    qy = models.FloatField()
+    is_metropolitan = models.BooleanField(default=False)
+    country = models.CharField(max_length=156)
+
+    def __str__(self):
+        return f"{self.name}-{self.country}"
