f2e1c70d964450c652e3da5c5bea516317dadfa2
Abdullah <125198163+abdullahdilekkk@users.noreply.github.com>
2025-08-19 09:24:46 +0300

    iki sayfa da beraber çalışıyor şu an

--- Demo/Map/urls.py
+++ Demo/Map/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('Turkey', views.turkey_only, name="TurkeyPage"),
-    path('Turkey/Istanbul', views.istanbul_only, name="IstanbulPage"),
+    path('Turkey/istanbul', views.istanbul_only, name="IstanbulPage"),
 ]
