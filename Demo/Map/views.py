e96586d96ccdc78ccbb0a9f15ac86f1897cba335
Abdullah <125198163+abdullahdilekkk@users.noreply.github.com>
2025-08-19 10:18:15 +0300

    map i bir base e taşıdım ve view da show_map metodu ile de tek bir gösteim sağladım

--- Demo/Map/views.py
+++ Demo/Map/views.py
@@ -4,13 +4,25 @@ from django.shortcuts import render, redirect
 import folium
 
 # Create your views here.
+
+def show_map(request, title, location, zoom=6):
+    m = folium.Map(
+        location=location,
+        zoom_start=zoom,
+        width="100%",
+        height="100%"
+    )
+    html = m._repr_html_()
+    return render(request, "Map/basicMap.html", {"map": html, "title": title})
+
+
 def turkey_only(request):
-    m = folium.Map(location=[39.0, 35.0], zoom_start=6, width="100%", height="600px")
-    html = m.get_root().render()
-    return render(request, "Map/Turkey.html", {"Turkey":html})
+    return show_map(request, "Turkey", [39.0, 35.0])
+    # m = folium.Map(location=[39.0, 35.0], zoom_start=6, width="100%", height="600px")
+    # html = m._repr_html_()
+    # return render(request, "Map/Turkey.html", {"Turkey":html})
 
 def istanbul_only(request):
-    m = folium.Map(location=[41.0082, 28.9784], zoom_start=10, width="100%", height="200px")
-    html = m._repr_html_()
-    return render (request, "Map/istanbul.html", {"Istanbul":html})
-
+    return show_map(request, "Istanbul", [41.0082, 28.9784], zoom=10)
+    
+   
