3a6e2fdf5f2b61e21ea75d72fcc22dfa0ae33775
Abdullah <125198163+abdullahdilekkk@users.noreply.github.com>
2025-08-22 12:02:50 +0300

    Hesaplar uygulaması eklendi; URL yönlendirmeleri güncellendi ve giriş sayfası oluşturuldu. fakat daha view da methodları yazılmadı

--- Demo/Demo/settings.py
+++ Demo/Demo/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'tailwind',
     'theme',
     'Map',
+    'accounts',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- Demo/Demo/urls.py
+++ Demo/Demo/urls.py
@@ -22,4 +22,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('Map/', include("Map.urls")),
+    path('accounts/',include("django.contrib.auth.urls")),#login ve logout default gelir bununla beraber
 ]
